cb5b4b8216be2c5c0350fd23dff667d71572fb90
Smirnov Denis <denis.smirnov@intel.com>
2017-09-04 09:08:04 +0300

    [Tests] Removed index.js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var _ = require("lodash");
-exports.getMessage = function () {
-    _.map(_.range(10), function (i) { return console.log(i); });
-    return "Hello World";
-};
-exports.getMessage();
